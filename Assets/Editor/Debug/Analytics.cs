999f703a64a6f8d59ceee7e609d49feb18729923
Enes Özdemir <enesozdemir6167@hotmail.com>
2022-12-31 22:07:11 +0300

    Assets folder removed

--- Assets/Editor/Debug/Analytics.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Editor.Debug
-{
-    public static class Analytics
-    {
-        public static void Log(object message) =>
-            UnityEngine.Debug.Log($"<color=lightblue><b><size=13>Analytics: </size></b></color>{message}");
-    }
-}
