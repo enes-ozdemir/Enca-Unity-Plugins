d9bdf8aa91d46b4b28fd7d595790915551601ace
Enes Özdemir <enesozdemir6167@hotmail.com>
2022-12-31 21:56:10 +0300

    Debug class added. Code refactored

---
+++ Assets/Editor/Debug/Log.cs
@@ -0,0 +1,23 @@
+﻿namespace Editor.Debug
+{
+    public static class Log
+    {
+        private static bool _showInfoLogs = true;
+
+        public static void Info(object message)
+        {
+            if (_showInfoLogs) UnityEngine.Debug.Log($"<color=turquoise>{message}</color>");
+        }
+
+        public static void Info(object message, bool showInfoLog)
+        {
+            if (showInfoLog) UnityEngine.Debug.Log($"<color=turquoise>{message}</color>");
+        }
+
+        public static void Error(object message) => UnityEngine.Debug.Log($"<color=red>{message}</color>");
+        public static void Warning(object message) => UnityEngine.Debug.Log($"<color=yellow>{message}</color>");
+
+        public static void ShowInfoLogs() => _showInfoLogs = true;
+        public static void HideInfoLogs() => _showInfoLogs = false;
+    }
+}
